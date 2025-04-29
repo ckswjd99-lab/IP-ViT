16677a48c0889b44217ea2c0ceacd21a378be3d4
sanghyun-snu <sanghyun@snu.ac.kr>
2025-04-29 17:15:28 +0900

    pipelining server pre, process and post

--- src/server.py
+++ src/server.py
@@ -29,55 +29,42 @@ from preprocessing import (
 )
 
 
-def thread_process_video(socket_rx: socket.socket, socket_tx: socket.socket, device: str='cuda') -> float:
+def thread_receive_video(socket_rx: socket.socket, socket_tx: socket.socket, frame_queue: Queue) -> float:
+    
     """
-    Thread function to process video frames received from the client and send results back.
+    Thread function to receive video frames from the client and put them into a queue.
 
     Args:
         socket_rx (socket.socket): The socket object to receive data from.
         socket_tx (socket.socket): The socket object to send data to.
+        frame_queue (Queue): Queue to store received frames.
     """
     import torch
-    from models import FasterRCNN_ResNet50_FPN_Contexted, MaskedRCNN_ViT_B_FPN_Contexted
 
-    # model = FasterRCNN_ResNet50_FPN_Contexted(device=device)
-    model = MaskedRCNN_ViT_B_FPN_Contexted(device=device)
-    model.load_weight("models/model_final_61ccd1.pkl")
-    model.eval()
-    print('Model loaded and ready to process frames.')
-
-    transmit_data(socket_tx, b"start!")
-
-    start_timestamp = time.time()
-
-    # Sizes: H, W
     input_size = (1024, 1024)
     frame_size = (854, 480)
 
     gop = 30
-    num_frames = 0
     anchor_image_padded = None
-    anchor_features = None
-    compute_rates = []
 
     while True:
         # Receive frame
         data = receive_data(socket_rx)
         if data is None:
-            transmit_data(socket_tx, b"", HEADER_TERMINATE)
             break
 
         fidx = int.from_bytes(data[:4], 'big')
-
         frame = bytes_to_ndarray(receive_data(socket_rx))
-        print(f"Received {fidx} | frame: {frame.shape} | timestamp: {time.time()}")
+        # print(f"Received {fidx} | frame: {frame.shape} | timestamp: {time.time()}")
 
         target_ndarray = cv2.resize(frame, frame_size)
 
         # Preprocess the frame
         refresh = False
-        if fidx % gop == 0 or anchor_image_padded is None or anchor_features is None:
+        if fidx % gop == 0 or anchor_image_padded is None:
             refresh = True
+            dirtiness_map = torch.ones(1, 64, 64, 1)
+            target_padded_ndarray = get_padded_image(target_ndarray, input_size)
         else:
             # try image refinement
             # affine_matrix = estimate_affine_in_padded_anchor(anchor_image_padded, frame)
@@ -89,6 +76,56 @@ def thread_process_video(socket_rx: socket.socket, socket_tx: socket.socket, dev
             
             refresh |= (target_padded_ndarray is None)
             refresh |= (scaling_factor < 0.95)
+        
+        anchor_image_padded = target_padded_ndarray
+
+        # Put the frame into the queue
+        frame_queue.put((fidx, refresh, target_ndarray, dirtiness_map.cpu().numpy()))
+        
+    frame_queue.put((-1, None, None, None))  # Send termination signal
+    
+
+def thread_process_video(
+    socket_rx: socket.socket, 
+    socket_tx: socket.socket, 
+    frame_queue: Queue, 
+    result_queue: Queue,
+    device: str='cuda'
+) -> float:
+    """
+    Thread function to process video frames received from the client and send results back.
+
+    Args:
+        socket_rx (socket.socket): The socket object to receive data from.
+        socket_tx (socket.socket): The socket object to send data to.
+    """
+    import torch
+    from models import MaskedRCNN_ViT_B_FPN_Contexted
+
+    model = MaskedRCNN_ViT_B_FPN_Contexted(device=device)
+    model.load_weight("models/model_final_61ccd1.pkl")
+    model.eval()
+    print('Model loaded and ready to process frames.')
+
+    transmit_data(socket_tx, b"start!")
+
+    start_timestamp = time.time()
+
+    # Sizes: H, W
+    input_size = (1024, 1024)
+
+    anchor_features = None
+    compute_rates = []
+
+    num_frames = 0
+
+    while True:
+        # Fetch frame
+        fidx, refresh, target_ndarray, dirtiness_map = frame_queue.get()
+        if fidx == -1:
+            break
+
+        dirtiness_map = torch.from_numpy(dirtiness_map).to(device)
         
         # Process the frame
         if refresh:
@@ -102,7 +139,6 @@ def thread_process_video(socket_rx: socket.socket, socket_tx: socket.socket, dev
                 dirtiness_map=dirtiness_map
             )
         
-        anchor_image_padded = target_padded_ndarray
         anchor_features = cached_features_dict
 
         cv2.imwrite(f"recv/{fidx:05d}.jpg", target_padded_ndarray)
@@ -110,11 +146,14 @@ def thread_process_video(socket_rx: socket.socket, socket_tx: socket.socket, dev
         # Statistics
         compute_rates.append(dirtiness_map.mean().item())
 
+        print(f"Processed {fidx} | boxes: {boxes.shape}, scores: {scores.shape}, labels: {labels.shape} | timestamp: {time.time()}")
+
         # Convert results to bytes
-        transmit_data(socket_tx, fidx.to_bytes(4, 'big'))
-        transmit_data(socket_tx, ndarray_to_bytes(boxes))
-        transmit_data(socket_tx, ndarray_to_bytes(scores))
-        transmit_data(socket_tx, ndarray_to_bytes(labels))
+        # transmit_data(socket_tx, fidx.to_bytes(4, 'big'))
+        # transmit_data(socket_tx, ndarray_to_bytes(boxes))
+        # transmit_data(socket_tx, ndarray_to_bytes(scores))
+        # transmit_data(socket_tx, ndarray_to_bytes(labels))
+        result_queue.put((fidx, boxes, scores, labels))
 
         num_frames += 1
 
@@ -123,7 +162,34 @@ def thread_process_video(socket_rx: socket.socket, socket_tx: socket.socket, dev
     print(f"Frame rate: {num_frames / (end_timestamp - start_timestamp)} FPS")
     print(f"Compute rate: {np.mean(compute_rates)}")
 
+    # transmit_data(socket_tx, b"", HEADER_TERMINATE)  # Send termination signal
+    result_queue.put((-1, None, None, None))  # Send termination signal
+
     return end_timestamp - start_timestamp
+
+
+def thread_send_result(
+    socket_rx: socket.socket, socket_tx: socket.socket, result_queue: Queue
+) -> float:
+    """
+    """
+
+    while True:
+        
+        # Receive results from the processing thread
+        fidx, boxes, scores, labels = result_queue.get()
+        if fidx == -1:
+            break
+
+        # Send results back to the client
+        transmit_data(socket_tx, fidx.to_bytes(4, 'big'))
+        transmit_data(socket_tx, ndarray_to_bytes(boxes))
+        transmit_data(socket_tx, ndarray_to_bytes(scores))
+        transmit_data(socket_tx, ndarray_to_bytes(labels))
+
+    transmit_data(socket_tx, b"", HEADER_TERMINATE)  # Send termination signal
+
+
 
 def main(args):
     server_ip = args.server_ip
@@ -138,8 +204,13 @@ def main(args):
     print(f"Timelag: {timelag} seconds")
 
     # Prepare processes
-    thread_proc = Process(target=thread_process_video, args=(socket_rx, socket_tx, device))
-    thread_proc.start()
+    frame_queue = Queue(maxsize=100)
+    result_queue = Queue(maxsize=100)
+
+    thread_recv = Process(target=thread_receive_video, args=(socket_rx, socket_tx, frame_queue))
+    thread_proc = Process(target=thread_process_video, args=(socket_rx, socket_tx, frame_queue, result_queue, device))
+    thread_send = Process(target=thread_send_result, args=(socket_rx, socket_tx, result_queue))
+    
 
     # Receive metadata
     metadata = receive_data(socket_rx)
@@ -148,9 +219,16 @@ def main(args):
     frame_rate = metadata.get("frame_rate", 30)
 
     print(f"Received metadata: {metadata}")
+    
 
+    thread_recv.start()
+    thread_proc.start()
+    thread_send.start()
+    
     # Wait for the processing thread to finish
+    thread_recv.join()
     thread_proc.join()
+    thread_send.join()
 
     # Close sockets
     socket_rx.close()
